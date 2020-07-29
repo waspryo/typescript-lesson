fd59517c608c97e8d97725bdaa9a62446f12d707
ldl093 <sugimoto@liddell.tokyo>
2020-07-29 18:32:19 +0900

    commit

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-"use strict";
-exports.__esModule = true;
-// axios.get("https://google/com");
-// // let hello: string = "hello";
-// // console.log(hello);
+// functionの型 ----------------------------------------------
 function add(a, b) {
     return a + b;
 }
 console.log(add(1, 3));
-console.log(aaaa);
+// 型指定 ----------------------------------------------
+var hasValue = true;
+var count = 10;
+var float = 2.14;
+var negative = -0.21;
+var single = "hello";
+var double = "hello";
+var back = "hello";
+// 型注釈、型推論 ----------------------------------------------
+// 型推論を基本使って型推論できないものは型注釈を使う
+// :booleanなどのついているものが型注釈
+var hasValues = true;
+// 型注釈を付けなくても推測してくれるものが型推論
+var word = "hello";
+// オブジェクトに型を付ける方法 ----------------------------------------------
+var person = {
+    name: "Jack",
+    age: 18
+};
+console.log(person.gender);
--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-// functionの型
+// functionの型 ----------------------------------------------
 function add(a: number, b: number): number {
   return a + b;
 }
 console.log(add(1, 3));
 
-// 型指定
+// 型指定 ----------------------------------------------
 let hasValue: boolean = true;
 let count: number = 10;
 let float: number = 2.14;
@@ -13,4 +13,23 @@ let single: string = "hello";
 let double: string = "hello";
 let back: string = `hello`;
 
-// 型注釈、型推論
+// 型注釈、型推論 ----------------------------------------------
+// 型推論を基本使って型推論できないものは型注釈を使う
+
+// :booleanなどのついているものが型注釈
+let hasValues: boolean = true;
+// 型注釈を付けなくても推測してくれるものが型推論
+let word = `hello`;
+
+// オブジェクトに型を付ける方法 ----------------------------------------------
+
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Jack",
+  age: 18,
+};
+console.log(person.name);
+
+// 配列に型を付けるArray型はこう使う ----------------------------------------------
