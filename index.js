bd4e18cd6e44cee8c28f479745a651deb24170a2
ldl093 <sugimoto@liddell.tokyo>
2020-07-29 18:19:15 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+// axios.get("https://google/com");
+// // let hello: string = "hello";
+// // console.log(hello);
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1, 3));
+console.log(aaaa);
---
+++ index.ts
@@ -0,0 +1,16 @@
+// functionの型
+function add(a: number, b: number): number {
+  return a + b;
+}
+console.log(add(1, 3));
+
+// 型指定
+let hasValue: boolean = true;
+let count: number = 10;
+let float: number = 2.14;
+let negative: number = -0.21;
+let single: string = "hello";
+let double: string = "hello";
+let back: string = `hello`;
+
+// 型注釈、型推論
