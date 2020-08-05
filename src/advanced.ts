0e63939e4c34ecfa64be51cccceefbaa602beb19
ldl093 <sugimoto@liddell.tokyo>
2020-08-06 00:08:02 +0900

    条件分絞り込み

--- src/advanced.ts
+++ src/advanced.ts
@@ -23,3 +23,46 @@ type StringNumber = string | number;
 type Mix = NumberBoolean & StringNumber;
 
 // 条件文を使って型を絞り込む、３つのType guard
+
+// typeof演算子
+
+function toUpperCase(x: string | number) {
+  if (typeof x === 'string') {
+    return x.toUpperCase();
+  }
+}
+
+// in演算子
+type NomadWorker = Engineer | Blogger;
+function describeProfile(nomadWorker: NomadWorker) {
+  // nameがどちらも持っているからアクセスできる
+  console.log(nomadWorker.name);
+  // 左にプロパティ名、キーの名前、右側にオブジェクト
+  if ('role' in nomadWorker) {
+    console.log(nomadWorker.role);
+  }
+}
+
+// instanseOf演算子
+class Dog {
+  speak() {
+    console.log('bow-wow');
+  }
+}
+
+class Bird {
+  speak() {
+    console.log('tweet-tweet ');
+  }
+  fly() {
+    console.log('flutter');
+  }
+}
+type Pet = Dog | Bird;
+function havePet(pet: Pet) {
+  pet.speak();
+  if (pet instanceof Bird) {
+    pet.fly();
+  }
+}
+havePet(new Bird());
